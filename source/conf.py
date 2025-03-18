07d418c382811b060d08f4c9ac8b7237f4c30c76
yuangn <yuangn@ccfund.com.cn>
2025-03-18 15:26:32 +0800

    fix: update funcions

--- source/conf.py
+++ source/conf.py
@@ -7,7 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'zhixin'
-copyright = '2024, YUAN'
+copyright = '2025, YUAN'
 author = 'YUAN'
 
 # -- General configuration ---------------------------------------------------
